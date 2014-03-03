a4b86a069aba3010a8c5a4b1c76bc6339cc5950d
Michele Orrù <maker@python.it>
2014-03-03 21:14:48 +0100

    Moving back continued fractions stuff to wiener.c.

--- src/questions/include/qarith.h
+++ src/questions/include/qarith.h
@@ -7,35 +7,6 @@
 
 #define BN_abs(a)  BN_set_negative(a, 0)
 
-
-/**
- * Fractions made of bignums.
- */
-typedef struct bigfraction {
-  BIGNUM* h;   /**< numerator */
-  BIGNUM* k;   /**< denominator */
-} bigfraction_t;
-
-
-typedef struct cf {
-  bigfraction_t fs[3];
-  short i;
-  bigfraction_t x;
-  BIGNUM* a;
-  BN_CTX* ctx;
-} cf_t;
-
-
-/* continued fractions utilities. */
-cf_t* cf_new(void);
-
-cf_t* cf_init(cf_t *f, BIGNUM *num, BIGNUM *b);
-
-void cf_free(cf_t* f);
-
-bigfraction_t* cf_next(cf_t *f);
-
-
 BIGNUM* BN_min(BIGNUM *a, BIGNUM *b);
 
 /* square root calculation */
--- src/questions/include/qwiener.h
+++ src/questions/include/qwiener.h
@@ -1,5 +1,32 @@
 #ifndef _QA_WIENER_H_
 #define _QA_WIENER_H
+/**
+ * Fractions made of bignums.
+ */
+typedef struct bigfraction {
+  BIGNUM* h;   /**< numerator */
+  BIGNUM* k;   /**< denominator */
+} bigfraction_t;
+
+
+typedef struct cf {
+  bigfraction_t fs[3];
+  short i;
+  bigfraction_t x;
+  BIGNUM* a;
+  BN_CTX* ctx;
+} cf_t;
+
+
+/* continued fractions utilities. */
+cf_t* cf_new(void);
+
+cf_t* cf_init(cf_t *f, BIGNUM *num, BIGNUM *b);
+
+void cf_free(cf_t* f);
+
+bigfraction_t* cf_next(cf_t *f);
+
 
 extern struct qa_question WienerQuestion;
 
