a515a5d047db4780c00bc19e97a9b11549c72dc0
Michele Orrù <maker@python.it>
2014-03-10 17:13:30 +0100

    Using a smaller dataset of primes, if not in cluster mode.
    Refine the script primes.sh to accept a parameter $1, the number of millions of
    primes that wants to be used as pool.
    Now, by default a simple user downloads only the first million of primes.
    A cluster user instead will download 20M primes.
    Please note that the maximum is 22 as of today.

--- src/apps/primes.sh
+++ src/apps/primes.sh
@@ -2,7 +2,7 @@
 
 > primes.txt
 
-for i in {1..16}
+for i in `seq $1`
 do
     curl "http://primes.utm.edu/lists/small/millions/primes$i.zip" | funzip - | \
         tail -n +3 | sed -E 's/[[:space:]]+/\n/g' -| sed -e '/^$/d'  >> primes.txt
